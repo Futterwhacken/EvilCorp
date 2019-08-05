6d0bbf13936f336db51bcdfc00784e858d50a761
HarmedChronogram <16294273+HarmedChronogram@users.noreply.github.com>
2019-08-06 00:22:45 +0200

    fixed immediate notification delay

--- src/evilcorp/graphic/gameobjects/text/NotificationImmediateArea.java
+++ src/evilcorp/graphic/gameobjects/text/NotificationImmediateArea.java
@@ -1,5 +1,6 @@
 package evilcorp.graphic.gameobjects.text;
 
+import evilcorp.graphic.Engine;
 import evilcorp.graphic.gfx.Font;
 import evilcorp.logic.NotificationBus;
 
@@ -9,30 +10,37 @@ public class NotificationImmediateArea extends TextArea
 {
     private final int color;
     private final double delay;
-    private final int[] counters;
+
+    private ArrayList<String> notifications;
+    private ArrayList<Double> timers;
 
     public NotificationImmediateArea(int posX, int posY, int width, int lineHeight, int maxLines, int color, Font font, double delay) {
         super(posX, posY, width, lineHeight, maxLines, font);
 
         this.color = color;
         this.delay = delay;
-        this.counters = new int[maxLines];
 
-        for (int i = 0; i < counters.length; i++) {
-            counters[i] = -1;
-        }
+        this.notifications = new ArrayList<>();
+        this.timers = new ArrayList<>();
     }
 
     @Override
     public void update() {
-        for (int i = 0; i < NotificationBus.getImmediateList().size() && i < counters.length; i++) {
-            if (counters[i] == -1) counters[i] = 0;
+        notifications = NotificationBus.getImmediateList();
 
-            counters[i]++;
+        if (notifications.size() == 0) {
+            timers = new ArrayList<>();
+        }
 
-            if (counters[i] > (1/engine.getUpdateRate()) * delay) {
+        while (timers.size() < notifications.size()) {
+            timers.add(Engine.getTime());
+        }
+
+        for (int i = 0; i < timers.size(); i++) {
+            if (Engine.getTime() - timers.get(i) >= delay) {
+                timers.remove(i);
                 NotificationBus.getImmediateList().remove(i);
-                counters[i] = -1;
+                //break;
             }
         }
     }
@@ -40,10 +48,9 @@ public class NotificationImmediateArea extends TextArea
 
     @Override @SuppressWarnings("Duplicates")
     public void render() {
-        int usedLines = 0;
-        ArrayList<String> notifications = NotificationBus.getImmediateList();
+        int lines = 0;
 
-        for (int i = notifications.size() - 1; i >= 0 && usedLines < maxLines; i--) {
+        for (int i = notifications.size() - 1; i >= 0 && lines < maxLines; i--) {
 
             String[] stringsArray = notifications.get(i).split("\n");
 
@@ -52,9 +59,9 @@ public class NotificationImmediateArea extends TextArea
                 strings.addAll(processString(s));
             }
 
-            for (int j = strings.size() - 1; j >= 0 && usedLines < maxLines; j--) {
-                engine.getRenderer().drawText(engine.getStandardFont(), strings.get(j), posX, posY + (lineHeight*maxLines) - (lineHeight*usedLines), color);
-                usedLines++;
+            for (int j = strings.size() - 1; j >= 0 && lines < maxLines; j--) {
+                engine.getRenderer().drawText(engine.getStandardFont(), strings.get(j), posX, posY + (lineHeight*maxLines) - (lineHeight*lines), color);
+                lines++;
             }
         }
     }
